e585b8238e46deb6e8ef94f769a480f29a5fb484
cuijd <coder.cui@qq.com>
2020-06-17 18:43:25 +0800

    udp

--- tcp-udp-tcp/src/main/java/org/jd/net/tut/Main.java
+++ tcp-udp-tcp/src/main/java/org/jd/net/tut/Main.java
@@ -16,23 +16,30 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] a) {
-
+        client(2000, "127.0.0.1", 2001);
     }
 
     /**
      * 启动客户端
      *
      * @param tcpListenPort 客户端tcp监听端口
-     * @param host          服务端
-     * @param port          服务端
+     * @param host          udp服务端 host
+     * @param port          udp服务端 port
      */
     static void client(int tcpListenPort, String host, int port) {
         ConcurrentHashMap<Integer, Channel> tcpMap = new ConcurrentHashMap<>();
         Channel udpServer = Netty.udp(port, new ChannelDuplexHandler() {
             @Override
             public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-                ctx.pipeline().addFirst(new PacketLostHandler());
+                ctx.pipeline().addBefore(ctx.name(), null, new PacketLostHandler());
                 super.handlerAdded(ctx);
+            }
+
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                if (msg instanceof ByteBuf)
+                    msg = new DatagramPacket((ByteBuf) msg, new InetSocketAddress(host, port));
+                super.write(ctx, msg, promise);
             }
 
             @Override
@@ -62,7 +69,7 @@ public class Main {
                                         Buf.wrap(channelIndex, dataIndexFactor.getAndAdd(1)),
                                         (ByteBuf) msg
                                 );
-                        udpServer.writeAndFlush(new DatagramPacket(buf, new InetSocketAddress(host, port)));
+                        udpServer.writeAndFlush(buf);
                     }
 
                     TreeSet<IndexBuf> indexBufs = new TreeSet<>();
@@ -84,7 +91,9 @@ public class Main {
                             }
                             that = next;
                             if (that.byteBuf.readableBytes() == 0) {//最后一个数据包
+                                ctx.flush();
                                 that.byteBuf.release();
+                                ctx.close();
                             }
                         }
                         indexBufs.removeAll(toRemove);//移除已消费
@@ -93,6 +102,7 @@ public class Main {
                     @Override
                     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
                         tcpMap.remove(channelIndex);
+                        udpServer.writeAndFlush(Buf.wrap(channelIndex, dataIndexFactor.getAndAdd(1)));//通知服务端数据结尾
                         super.channelInactive(ctx);
                     }
                 });
@@ -104,10 +114,35 @@ public class Main {
      * 启动服务端
      *
      * @param udpListenPort 服务端udp监听端口
-     * @param host
-     * @param port
+     * @param host tcp 服务端 host
+     * @param port tcp 服务端 port
      */
     static void server(int udpListenPort, String host, int port) {
+        ConcurrentHashMap<Integer, Channel> tcpMap = new ConcurrentHashMap<>();
+        Channel udpServer = Netty.udp(udpListenPort, new ChannelDuplexHandler() {
+            @Override
+            public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+                ctx.pipeline().addBefore(ctx.name(), null, new PacketLostHandler());
+                super.handlerAdded(ctx);
+            }
 
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                if (msg instanceof ByteBuf)
+                    msg = new DatagramPacket((ByteBuf) msg, new InetSocketAddress(host, port));
+                super.write(ctx, msg, promise);
+            }
+
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                ByteBuf buf = ((DatagramPacket) msg).content();
+                Channel tcp = tcpMap.get(buf.readInt());//根据channelIndex选择合适的tcp连接
+                if (tcp != null) {
+                    tcp.writeAndFlush(buf);
+                } else {
+                    buf.release();
+                }
+            }
+        }).syncUninterruptibly().channel();
     }
 }
