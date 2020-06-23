645bcd3974ad4b79b42e8c69d4f1aeb6342b315f
cuijd <coder.cui@qq.com>
2020-06-23 18:31:09 +0800

    udp

---
+++ app/tcp-bridge/src/main/java/org/jd/tcp/bridge/Main.java
@@ -0,0 +1,26 @@
+package org.jd.tcp.bridge;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] a) {
+        if ("-s".equalsIgnoreCase(a[0])) {// -s port
+            serverStart(Integer.valueOf(a[1]), a[2]);
+        } else {// port serverHost serverPort password
+            clientStart(Integer.valueOf(a[0]), a[1], Integer.valueOf(a[2]), a[3]);
+        }
+    }
+
+    static void clientStart(int port, String sHost, int sPort, String password) {
+
+
+    }
+
+    static void serverStart(int port, String password) {
+
+    }
+
+}
